b6803555dad32a90735e6b91b058435598e2ddbf
Dhairya <sharmadhairya024@gmail.com>
2026-05-12 12:36:53 +0530

    Refactor production DB config to use MYSQL_URL
    Updated production database configuration to use environment variable for connection URL and added connection timeout options.

--- server/config/database.js
+++ server/config/database.js
@@ -10,11 +10,11 @@ module.exports = {
     logging: false,
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
-    logging: false,
+  use_env_variable: 'MYSQL_URL',
+  dialect: 'mysql',
+  logging: false,
+  dialectOptions: {
+    connectTimeout: 60000,
   }
+}
 }
